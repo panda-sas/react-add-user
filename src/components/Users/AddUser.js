9b47ead6c4253980a77b98f2f97b227b870ad4b6
panda-sas <panda.sas20@gmail.com>
2021-08-20 17:29:12 -0500

    Added ChangeHandlers for username and age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../UI/Button";
 import Card from "../UI/Card";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setenteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername, enteredAge);
   };
 
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHandler = (event) => {
+    setenteredAge(event.target.value);
+  };
   return (
-    <Card className= {classes.input}>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input id="username" type="text" onChange={usernameChangeHandler} />
         <label htmlFor="age">Age (Years)</label>
-        <input id="age" type="number" />
-        <button type="submit">Add User</button>
+        <input id="age" type="number" onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
